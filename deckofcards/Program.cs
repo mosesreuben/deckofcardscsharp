faa2a52135ff4af3ea274bbb77e0702cff5bfa1c
Moses Reuben <46658412+mosesreuben@users.noreply.github.com>
2019-04-08 15:02:39 -0700

    Add files via upload

---
+++ deckofcards/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace deckofcards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck stack = new Deck();
+            Player Bob = new Player("Bob");
+            Console.WriteLine(stack.cards.Count);
+        
+            Bob.draw(stack);
+            Console.WriteLine(Bob.hand.Count);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);
+            Bob.draw(stack);            
+        }
+    }
+}
